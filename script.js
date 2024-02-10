351709564cb1d7c1877b1c749f95785c3fa56547
abushahid81 <abushahidsardar81@gmail.com>
2024-02-10 14:16:42 +0530

    js

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ let hours = document.getElementById('hours');
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 
-setInterval(()=>{
+setInterval(function(){
 
     var currentTime = new Date();
 
-     hours.innerHTML = currentTime.getHours();
-     minutes.innerHTML = currentTime.getMinutes();
-     seconds.innerHTML = currentTime.getSeconds();
-
+     hours.innerHTML = currentTime.getHours("hours");
+     minutes.innerHTML = currentTime.getMinutes("minutes");
+     seconds.innerHTML = currentTime.getSeconds("seconds");
+     
 },1000);
+
